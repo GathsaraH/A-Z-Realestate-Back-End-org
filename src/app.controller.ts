df99d978c0faca17db29d7e34508f4442866afac
Gathsara <66924429+GathsaraH@users.noreply.github.com>
2023-07-13 00:05:49 +0530

    add wagger tag

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiExcludeController } from '@nestjs/swagger';
-@ApiExcludeController()
+import { ApiExcludeController, ApiTags } from '@nestjs/swagger';
+//@ApiExcludeController()
+@ApiTags('Health Check E/P')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}

ccbd3c8067bdb56daf2251b391a0478656efb6ee
Gathsara <66924429+GathsaraH@users.noreply.github.com>
2023-07-16 14:42:56 +0530

    feat:define cron service

--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { CronService } from './cron.service';
 import { CronController } from './cron.controller';
 import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LeaseEntity } from 'src/cloud-az/entities/lease.entity';
+import { LeaseSyncEntity } from 'src/cloud-az/entities/lease-sync.entity';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [ScheduleModule.forRoot(),TypeOrmModule.forFeature([LeaseEntity,LeaseSyncEntity])],
   controllers: [CronController],
   providers: [CronService],
 })
--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
-export class CronService {}
+export class CronService {
+  private readonly logger = new Logger(CronService.name);
+
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  async syncSalePropertyData() {
+    try {
+      this.logger.debug(
+        `Sync SaleProperty Data at : ${new Date().toISOString()}`,
+      );
+    } catch (error) {
+      throw new HttpException(error.message, 400);
+    }
+  }
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  async syncSoldPropertyData() {
+    try {
+      this.logger.debug(
+        `Sync SoldProperty Data at : ${new Date().toISOString()}`,
+      );
+    } catch (error) {
+      throw new HttpException(error.message, 400);
+    }
+  }
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  async syncLeasePropertyData() {
+    try {
+      this.logger.debug(
+        `Sync LeaseProperty Data at : ${new Date().toISOString()}`,
+      );
+    } catch (error) {
+      throw new HttpException(error.message, 400);
+    }
+  }
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  async syncReviewsData() {
+    try {
+      this.logger.debug(`Sync Reviews Data at : ${new Date().toISOString()}`);
+    } catch (error) {
+      throw new HttpException(error.message, 400);
+    }
+  }
+}

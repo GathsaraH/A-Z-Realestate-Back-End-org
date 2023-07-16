32e3ed4d73c736e9483356354b5d4988245be571
Gathsara <66924429+GathsaraH@users.noreply.github.com>
2023-07-16 22:22:33 +0530

    feat:add imports

--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -10,10 +10,27 @@ import { CloudAzModule } from 'src/cloud-az/cloud-az.module';
 import { SoldEntity } from 'src/entities/sold.entity';
 import { SoldSyncEntity } from 'src/entities/sold-sync.entity';
 import { DataProcessingModule } from 'src/data-processing/data-processing.module';
-
+import { ReviewsEntity } from 'src/entities/reviews.entity';
+import { ReviewsSyncEntity } from 'src/entities/reviews-sync.entity';
+import { SaleEntity } from 'src/entities/sale.entity';
+import { SaleSyncEntity } from 'src/entities/sale-sync.entity.ts';
 
 @Module({
-  imports: [ScheduleModule.forRoot(),TypeOrmModule.forFeature([SoldEntity,SoldSyncEntity]),HttpUtilModule,DataProcessingModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    TypeOrmModule.forFeature([
+      SoldEntity,
+      SoldSyncEntity,
+      LeaseEntity,
+      LeaseSyncEntity,
+      ReviewsEntity,
+      ReviewsSyncEntity,
+      SaleEntity,
+      SaleSyncEntity,
+    ]),
+    HttpUtilModule,
+    DataProcessingModule,
+  ],
   controllers: [CronController],
   providers: [CronService],
 })
